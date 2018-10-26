961b81ef771ed9f2782f9c761ed3d31f332025e9
Nicolas Riesco <enquiries@nicolasriesco.net>
2018-10-26 13:00:05 +0100

    Refactor code to build curves into functions
    * Fixed exception triggered when any expected curves are missing.

--- lastodash.py
+++ lastodash.py
@@ -3,21 +3,21 @@
 
 
 # Copyright 2018 Nicolas Riesco
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
-# 
+#
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 # this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
-# 
+#
 # 3. Neither the name of the copyright holder nor the names of its contributors
 # may be used to endorse or promote products derived from this software without
 # specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -76,7 +76,7 @@ def build_layout(lasfile):
     las = lasio.read(lasfile)
 
     return html.Div([
-        html.H1('LAS Viewer'),
+        html.H1('LAS Report'),
         build_section_version(las, lasfile),
         build_section_well(las),
         build_section_curves(las)
@@ -130,201 +130,251 @@ def build_section_well_entry(entry):
 
 
 def build_section_curves(las):
+    graphs = []
+
+    dgr = build_graph_dgr(las)
+    if dgr is not None:
+        graphs.append(dgr)
+
+    rp = build_graph_rp(las)
+    if rp is not None:
+        graphs.append(rp)
+
+    ald = build_graph_ald(las)
+    if ald is not None:
+        graphs.append(ald)
+
+    ctn = build_graph_ctn(las)
+    if ctn is not None:
+        graphs.append(ctn)
+
+    bat = build_graph_bat(las)
+    if bat is not None:
+        graphs.append(bat)
+
+    return html.Div(
+        [html.H2('Section: curves')] + graphs,
+        className='las-curves'
+    )
+
+
+SCATTER_LINE_LINE_BLACK = dict(color='black', width=1)
+SCATTER_LINE_LINE_RED = dict(color='red', width=1)
+SCATTER_LINE_LINE_GREEN = dict(color='green', width=1)
+SCATTER_LINE_LINE_BLUE = dict(color='blue', width=1)
+SCATTER_LINE_LINE_MAGENTA = dict(color='magenta', width=1)
+
+SCATTER_LINE_DASH_BLACK = dict(color='black', dash='dash', width=1)
+SCATTER_LINE_DASH_RED = dict(color='red', dash='dash', width=1)
+SCATTER_LINE_DASH_GREEN = dict(color='green', dash='dash', width=1)
+SCATTER_LINE_DASH_BLUE = dict(color='blue', dash='dash', width=1)
+SCATTER_LINE_DASH_MAGENTA = dict(color='magenta', dash='dash', width=1)
+
+SCATTER_LINE_DOT_BLACK = dict(color='black', dash='dot', width=1)
+SCATTER_LINE_DOT_RED = dict(color='red', dash='dot', width=1)
+SCATTER_LINE_DOT_GREEN = dict(color='green', dash='dot', width=1)
+SCATTER_LINE_DOT_BLUE = dict(color='blue', dash='dot', width=1)
+SCATTER_LINE_DOT_MAGENTA = dict(color='magenta', dash='dot', width=1)
+
+SCATTER_LINE_DASHDOT_BLACK = dict(color='black', dash='dashdot', width=1)
+SCATTER_LINE_DASHDOT_RED = dict(color='red', dash='dashdot', width=1)
+SCATTER_LINE_DASHDOT_GREEN = dict(color='green', dash='dashdot', width=1)
+SCATTER_LINE_DASHDOT_BLUE = dict(color='blue', dash='dashdot', width=1)
+SCATTER_LINE_DASHDOT_MAGENTA = dict(color='magenta', dash='dashdot', width=1)
+
+
+def build_graph_dgr(las):
+    data, yaxis, yaxis2 = [], None, None
+
+    if 'BTVPVS' in las.curves:
+        btvpvs_trace, btvpvs_axis = build_trace(
+            las.curves['BTVPVS'],
+            line=SCATTER_LINE_LINE_MAGENTA,
+            yaxis='y'
+        )
+        data.append(btvpvs_trace)
+        yaxis = btvpvs_axis
+
+    if 'DGRC' in las.curves:
+        dgrc_trace, dgrc_axis = build_trace(
+            las.curves['DGRC'],
+            line=SCATTER_LINE_LINE_GREEN,
+            yaxis='y2' if yaxis else 'y'
+        )
+        data.append(dgrc_trace)
+        if yaxis:
+            yaxis2 = dgrc_axis
+        else:
+            yaxis = dgrc_axis
+
+    return build_graph('las-curves-dgr', las, data, yaxis, yaxis2)
+
+
+def build_graph_rp(las):
+    data, yaxis, yaxis2 = [], None, None
+
+    if 'EWXT' in las.curves:
+        ewxt_trace, ewxt_axis = build_trace(
+            las.curves['EWXT'],
+            line=SCATTER_LINE_DASHDOT_MAGENTA,
+            yaxis='y'
+        )
+        ewxt_axis['type'] = 'log'
+        data.append(ewxt_trace)
+        yaxis = ewxt_axis
+
+    rp_axis = dict(
+        title='RxxP [ohmm]',
+        type='log'
+    )
+    rp_keys = ['R09P', 'R15P', 'R27P', 'R39P']
+    rp_lines = [
+        SCATTER_LINE_DASHDOT_GREEN,
+        SCATTER_LINE_LINE_BLACK,
+        SCATTER_LINE_DOT_BLUE,
+        SCATTER_LINE_DASH_RED
+    ]
+    has_rp = False
+
+    for key, line in zip(rp_keys, rp_lines):
+        if key in las.curves:
+            rp_trace, _ = build_trace(
+                las.curves[key],
+                line=line,
+                yaxis='y2' if yaxis else 'y'
+            )
+            data.append(rp_trace)
+            has_rp = True
+
+    if has_rp:
+        if yaxis:
+            yaxis2 = rp_axis
+        else:
+            yaxis = rp_axis
+
+    return build_graph('las-curves-rp', las, data, yaxis, yaxis2)
+
+
+def build_graph_ald(las):
+    data, yaxis, yaxis2 = [], None, None
+
+    if 'ALDCLC' in las.curves:
+        aldclc_trace, aldclc_axis = build_trace(
+            las.curves['ALDCLC'],
+            line=SCATTER_LINE_DASH_BLACK,
+            yaxis='y'
+        )
+        data.append(aldclc_trace)
+        yaxis = aldclc_axis
+
+    if 'ALCDLC' in las.curves:
+        alcdlc_trace, alcdlc_axis = build_trace(
+            las.curves['ALCDLC'],
+            line=SCATTER_LINE_LINE_RED,
+            yaxis='y2' if yaxis else 'y'
+        )
+        data.append(alcdlc_trace)
+        if yaxis:
+            yaxis2 = alcdlc_axis
+        else:
+            yaxis = alcdlc_axis
+
+    return build_graph('las-curves-ald', las, data, yaxis, yaxis2)
+
+
+def build_graph_ctn(las):
+    data, yaxis = [], None
+
+    if 'TNPS' in las.curves:
+        tnps_trace, tnps_axis = build_trace(
+            las.curves['TNPS'],
+            line=SCATTER_LINE_LINE_BLUE,
+            yaxis='y'
+        )
+        data.append(tnps_trace)
+        yaxis = tnps_axis
+
+    return build_graph('las-curves-ctn', las, data, yaxis)
+
+
+def build_graph_bat(las):
+    data, yaxis, yaxis2 = [], None, None
+
+    if 'BTCSS' in las.curves:
+        btcss_trace, btcss_axis = build_trace(
+            las.curves['BTCSS'],
+            line=SCATTER_LINE_LINE_MAGENTA,
+            yaxis='y'
+        )
+        data.append(btcss_trace)
+        yaxis = btcss_axis
+
+    if 'BTCS' in las.curves:
+        btcs_trace, btcs_axis = build_trace(
+            las.curves['BTCS'],
+            line=SCATTER_LINE_LINE_GREEN,
+            yaxis='y2' if yaxis else 'y'
+        )
+        data.append(btcs_trace)
+        if yaxis:
+            yaxis2 = btcs_axis
+        else:
+            yaxis = btcs_axis
+
+    return build_graph('las-curves-bat', las, data, yaxis, yaxis2)
+
+
+def build_trace(curve, **kwargs):
+    key = curve.original_mnemonic
+    data = curve.data
+    unit = ' [{0}]'.format(curve.unit) if curve.unit != 'NONE' else ''
+    descr = curve.descr
+
+    trace = go.Scatter(
+        y=data,
+        mode='lines',
+        name=descr,
+        **kwargs
+    )
+
+    axis = dict(
+        title=key+unit
+    )
+
+    return trace, axis
+
+
+def build_graph(id, las, data, yaxis=None, yaxis2=None):
+    if not data:
+        return None
+
     x = las.depth_ft
     xaxis = dict(
         title='depth [ft]'
     )
 
-    line_black = dict(color='black', width=1)
-    line_red = dict(color='red', width=1)
-    line_green = dict(color='green', width=1)
-    line_blue = dict(color='blue', width=1)
-    line_magenta = dict(color='magenta', width=1)
+    for trace in data:
+        trace['x'] = x
 
-    dash_black = dict(color='black', dash='dash', width=1)
-    dash_red = dict(color='red', dash='dash', width=1)
-    dash_green = dict(color='green', dash='dash', width=1)
-    dash_blue = dict(color='blue', dash='dash', width=1)
-    dash_magenta = dict(color='magenta', dash='dash', width=1)
+    if yaxis2 is not None:
+        # since yaxis2 is drawn on top of yaxis
+        yaxis['side'] = 'right'
+        yaxis['showgrid'] = False
 
-    dot_black = dict(color='black', dash='dot', width=1)
-    dot_red = dict(color='red', dash='dot', width=1)
-    dot_green = dict(color='green', dash='dot', width=1)
-    dot_blue = dict(color='blue', dash='dot', width=1)
-    dot_magenta = dict(color='magenta', dash='dot', width=1)
-
-    dashdot_black = dict(color='black', dash='dashdot', width=1)
-    dashdot_red = dict(color='red', dash='dashdot', width=1)
-    dashdot_green = dict(color='green', dash='dashdot', width=1)
-    dashdot_blue = dict(color='blue', dash='dashdot', width=1)
-    dashdot_magenta = dict(color='magenta', dash='dashdot', width=1)
-
-
-    btvpvs_axis, btvpvs_trace = build_plot(
-        las, 'BTVPVS', line=line_magenta, x=x, yaxis='y'
+    # Note: xaxis, yaxis and yaxis2 must be set before creating layout.
+    layout = go.Layout(
+        hovermode='x',
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        showlegend=True,
+        xaxis=xaxis,
+        yaxis=yaxis
     )
-    btvpvs_axis['side'] = 'right'
-    btvpvs_axis['showgrid'] = False
-    dgrc_axis, dgrc_trace = build_plot(
-        las, 'DGRC', line=line_green, x=x, yaxis='y2'
-    )
-    dgr_graph = dcc.Graph(figure={
-        'data': [
-            btvpvs_trace,
-            dgrc_trace
-        ],
-        'layout': go.Layout(
-            hovermode='x',
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)',
-            showlegend=True,
-            xaxis=xaxis,
-            yaxis=btvpvs_axis,
-            yaxis2=dgrc_axis
-        )
-    }, id='las-curves-dgr')
 
-    ewxt_axis, ewxt_trace = build_plot(
-        las, 'EWXT', line=dashdot_magenta, x=x, yaxis='y'
-    )
-    ewxt_axis['type'] = 'log'
-    ewxt_axis['side'] = 'right'
-    ewxt_axis['showgrid'] = False
-    r09p_axis, r09p_trace = build_plot(
-        las, 'R09P', line=dashdot_green, x=x, yaxis='y2'
-    )
-    r15p_axis, r15p_trace = build_plot(
-        las, 'R15P', line=line_black, x=x, yaxis='y2'
-    )
-    r27p_axis, r27p_trace = build_plot(
-        las, 'R27P', line=dot_blue, x=x, yaxis='y2'
-    )
-    r39p_axis, r39p_trace = build_plot(
-        las, 'R39P', line=dash_red, x=x, yaxis='y2'
-    )
-    rp_axis = dict(
-        title='RxxP [ohmm]',
-        type='log'
-    )
-    rp_graph = dcc.Graph(figure={
-        'data': [
-            ewxt_trace,
-            r09p_trace,
-            r15p_trace,
-            r27p_trace,
-            r39p_trace
-        ],
-        'layout': go.Layout(
-            hovermode='x',
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)',
-            showlegend=True,
-            xaxis=xaxis,
-            yaxis=ewxt_axis,
-            yaxis2=rp_axis
-        )
-    }, id='las-curves-rp')
+    if yaxis2 is not None:
+        layout['yaxis2'] = yaxis2
 
-    aldclc_axis, aldclc_trace = build_plot(
-        las, 'ALDCLC', line=dash_black, x=x, yaxis='y'
-    )
-    alcdlc_axis, alcdlc_trace = build_plot(
-        las, 'ALCDLC', line=line_red, x=x, yaxis='y2'
-    )
-    alcdlc_axis['side'] = 'right'
-    alcdlc_axis['showgrid'] = False
-    ald_graph = dcc.Graph(figure={
-        'data': [
-            alcdlc_trace,
-            aldclc_trace
-        ],
-        'layout': go.Layout(
-            hovermode='x',
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)',
-            showlegend=True,
-            xaxis=xaxis,
-            yaxis=aldclc_axis,
-            yaxis2=alcdlc_axis
-        )
-    }, id='las-curves-ald')
-
-    tnps_axis, tnps_trace = build_plot(
-        las, 'TNPS', line=line_blue, x=x, yaxis='y'
-    )
-    ctn_graph = dcc.Graph(figure={
-        'data': [
-            tnps_trace
-        ],
-        'layout': go.Layout(
-            hovermode='x',
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)',
-            showlegend=True,
-            xaxis=xaxis,
-            yaxis=tnps_axis
-        )
-    }, id='las-curves-ctn')
-
-    btcss_axis, btcss_trace = build_plot(
-        las, 'BTCSS', line=line_magenta, x=x, yaxis='y'
-    )
-    btcs_axis, btcs_trace = build_plot(
-        las, 'BTCS', line=line_green, x=x, yaxis='y2'
-    )
-    btcs_axis['side'] = 'right'
-    btcs_axis['showgrid'] = False
-    bat_graph = dcc.Graph(figure={
-        'data': [
-            btcss_trace,
-            btcs_trace
-        ],
-        'layout': go.Layout(
-            hovermode='x',
-            paper_bgcolor='rgba(0,0,0,0)',
-            plot_bgcolor='rgba(0,0,0,0)',
-            showlegend=True,
-            xaxis=xaxis,
-            yaxis=btcss_axis,
-            yaxis2=btcs_axis
-        )
-    }, id='las-curves-bat')
-
-    return html.Div([
-        html.H2('Section: curves'),
-        dgr_graph,
-        rp_graph,
-        ald_graph,
-        ctn_graph,
-        bat_graph
-    ], className='las-curves')
-
-
-def build_plot(las, key, **kwargs):
-    axis = None
-    trace = None
-
-    try:
-        curve = las.curves[key]
-        data = curve.data
-        unit = ' [{0}]'.format(curve.unit) if curve.unit != 'NONE' else ''
-        descr = curve.descr
-
-        axis = dict(
-            title=key+unit
-        )
-
-        trace = go.Scatter(
-            y=data,
-            mode='lines',
-            name=descr,
-            **kwargs
-        )
-
-    except Exception as e:
-        print('Warning: Missing curve {0}: {1}'.format(key, e))
-
-    return axis, trace
+    return dcc.Graph(figure={'data': data, 'layout': layout}, id=id)
 
 
 if __name__ == '__main__':
