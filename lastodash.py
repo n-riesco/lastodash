a98fcf0bcedd2eb9298ecc085c7fc022986c3688
Nicolas Riesco <enquiries@nicolasriesco.net>
2018-11-06 23:34:57 +0000

    Add page UI
    * Added print button.
    
    * Added dropdown to select page size (A4 and letter).
    
    * Added dropdown to select view mode (single page and all pages).
    
    * Added buttons and input box to move to another page.
    
    * Fixed extra blank pages when printing with Firefox.

--- lastodash.py
+++ lastodash.py
@@ -35,6 +35,7 @@ import argparse
 import os
 
 import dash
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 
@@ -47,25 +48,32 @@ def main():
     lasfile, debug = parse_args()
 
     app = dash.Dash(__name__)
-    app.layout = build_layout(lasfile)
+
+    app.css.config.serve_locally = True
+    app.scripts.config.serve_locally = True
+
+    layout, page_count = build_layout(lasfile)
+    app.layout = layout
+    setup_callbacks(app, page_count)
+
     app.run_server(debug=debug)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Launch a Dash app to view a LAS log.'
+        description="Launch a Dash app to view a LAS log."
     )
 
     parser.add_argument(
-        'lasfile',
-        type=argparse.FileType(mode='r'),
-        help='Log ASCII Standard (LAS) file'
+        "lasfile",
+        type=argparse.FileType(mode="r"),
+        help="Log ASCII Standard (LAS) file"
     )
 
     parser.add_argument(
-        '--debug', '-d',
-        action='store_true',
-        help='enable debug mode'
+        "--debug", "-d",
+        action="store_true",
+        help="enable debug mode"
     )
 
     args = parser.parse_args()
@@ -76,42 +84,184 @@ def parse_args():
 def build_layout(lasfile):
     las = lasio.read(lasfile)
 
-    filename = os.path.basename(lasfile.name)
-    description = (
-        '({0})'.format(las.version['VERS'].descr)
-        if 'VERS' in las.version
-        else '(unknown version)'
-    )
+    # build header
+    header = build_header(las, lasfile)
 
-    header = [
-        html.H1('LAS Report'),
-        html.Div([
-            html.B(filename, className='las-header-filename'),
-            html.Span(description, className='las-header-description')
-        ], className='las-header')
+    # build pages
+    page_start = 1
+    well = [
+        build_page(
+            contents,
+            header,
+            label="well",
+            page_number=page_start+i
+        )
+        for i, contents in enumerate(build_section_well(las))
     ]
 
-    layout = []
-    layout += build_section_well(las, header)
-    layout += build_section_curves(las, header)
+    page_start += len(well)
+    curves = [
+        build_page(
+            contents,
+            header,
+            label="curves",
+            page_number=page_start+i
+        )
+        for i, contents in enumerate(build_section_curves(las))
+    ]
 
-    return html.Div(layout, className='las')
+    page_count = len(well) + len(curves)
+
+    # build navigation controls
+    nav = build_nav(page_count)
+
+    # build layout
+    layout = html.Div(well + curves + [nav], className="a4", id="las")
+
+    return layout, page_count
 
 
-def build_section_well(las, header):
-    pages = paginate(
-        (build_section_well_entry(e) for e in las.well),
-        items_per_page=50
+def build_nav(page_count):
+    button_print = html.Button("Print", id="las-nav-print")
+
+    dropdown_page_size = dcc.Dropdown(
+        id="las-nav-size",
+        options=[
+            {"label": "Page size: A4", "value": "a4"},
+            {"label": "Page size: letter", "value": "letter"}
+        ],
+        value="a4",
+        clearable=False
     )
 
+    dropdown_view_mode = dcc.Dropdown(
+        id="las-nav-view",
+        options=[
+            {"label": "Show only one page", "value": "one"},
+            {"label": "Show all pages", "value": "all"}
+        ],
+        value="one",
+        clearable=False
+    )
+
+    input_page_index = dcc.Input(
+        id="las-nav-index",
+        type="number",
+        min=1,
+        max=page_count,
+        value=1
+    )
+
+    button_first_page = html.Button("<<", id="las-nav-move-first")
+    button_prev_page = html.Button("<", id="las-nav-move-prev")
+    button_next_page = html.Button(">", id="las-nav-move-next")
+    button_last_page = html.Button(">>", id="las-nav-move-last")
+
+    nav = html.Div([
+        button_print,
+        dropdown_page_size,
+        dropdown_view_mode,
+        html.Div([
+            button_first_page,
+            button_prev_page,
+            input_page_index,
+            button_next_page,
+            button_last_page
+        ], id="las-nav-move")
+    ], id="las-nav")
+
+    return nav
+
+
+def setup_callbacks(app, page_count):
+    # Callback to set the page size
+    @app.callback(Output("las", "className"),
+                  [Input("las-nav-size", "value")])
+    def on_page_size_change(page_size):
+        return page_size
+
+    # Callback to set page visibility
+    for i in range(1, 1+page_count):
+        @app.callback(Output("page{}".format(i), "style"),
+                      [Input("las-nav-index", "value"),
+                       Input("las-nav-view", "value")])
+        def on_page_visibility_change(index, view, i=i):
+            if index < 1:
+                index = 1
+            elif index > page_count:
+                index = page_count
+            if view == "all" or index == i:
+                return {"display": "block"}
+
+            return {"display": "none"}
+
+    # Callbacks to update page index
+    @app.callback(Output("las-nav-index", "value"),
+                  [Input("las-nav-move-first", "n_clicks_timestamp"),
+                   Input("las-nav-move-prev", "n_clicks_timestamp"),
+                   Input("las-nav-move-next", "n_clicks_timestamp"),
+                   Input("las-nav-move-last", "n_clicks_timestamp")],
+                  [State("las-nav-index", "value")])
+    def on_click_move(first, prev, next, last, index):
+        timestamp = max(first or 0, prev or 0, next or 0, last or 0)
+        if timestamp is last:
+            return page_count
+        elif timestamp is next:
+            return min(index+1, page_count)
+        elif timestamp is prev:
+            return max(index-1, 1)
+        return 1
+
+    # Callback to show/hide page navigation UI
+    @app.callback(Output("las-nav-move", "style"),
+                  [Input("las-nav-view", "value")])
+    def on_page_view_change(view_mode):
+        if view_mode == "all":
+            return {"display": "none"}
+
+        return {"display": "flex"}
+
+
+def build_header(las, lasfile):
+    filename = os.path.basename(lasfile.name)
+    description = (
+        "({0})".format(las.version["VERS"].descr)
+        if "VERS" in las.version
+        else "(unknown version)"
+    )
+
+    subheading = html.Div([
+        html.B(filename, className="las-header-filename"),
+        html.Span(description, className="las-header-description")
+    ], className="las-header-subheading")
+
+    heading = html.H1("LAS Report", className="las-header-heading")
+
+    return html.Div([
+        heading,
+        subheading
+    ], className="las-header")
+
+
+def build_page(contents, header, label, page_number):
+    className = "las-" + label
+    id = "page{}".format(page_number)
+    title = "Section: {} (page {})".format(label, page_number)
+
+    return html.Div([
+        header,
+        html.H2(title),
+        contents
+    ], className=className+" page", id=id)
+
+
+def build_section_well(las):
     return [
-        build_page(
-            header,
-            title='Section: well (page {0})'.format(i+1),
-            contents=html.Table(page),
-            className='las-well'
+        html.Table(page)
+        for page in paginate(
+            (build_section_well_entry(e) for e in las.well),
+            items_per_page=50
         )
-        for i, page in enumerate(pages)
     ]
 
 
@@ -120,21 +270,21 @@ def build_section_well_entry(entry):
         html.Td([
             html.B(
                 entry.descr,
-                className='las-well-entry-description'
+                className="las-well-entry-description"
             ),
             html.Span(
-                '[{0}]'.format(entry.unit) if entry.unit else '',
-                className='las-well-entry-unit'
+                "[{0}]".format(entry.unit) if entry.unit else "",
+                className="las-well-entry-unit"
             ),
         ]),
         html.Td(
             entry.value,
-            className='las-well-entry-value'
+            className="las-well-entry-value"
         )
-    ], className='las-well-entry')
+    ], className="las-well-entry")
 
 
-def build_section_curves(las, header):
+def build_section_curves(las):
     graphs = []
 
     dgr = build_graph_dgr(las)
@@ -157,64 +307,56 @@ def build_section_curves(las, header):
     if bat is not None:
         graphs.append(bat)
 
-    pages = paginate(
-        graphs,
-        items_per_page=2
-    )
-
-    return [
-        build_page(
-            header,
-            title='Section: curves (page {0})'.format(i+1),
-            contents=html.Div(page),
-            className='las-curves'
-        )
-        for i, page in enumerate(pages)
+    pages = [
+        html.Div(page)
+        for page in paginate(graphs, items_per_page=2)
     ]
 
+    return pages
 
-SCATTER_LINE_LINE_BLACK = dict(color='black', width=1)
-SCATTER_LINE_LINE_RED = dict(color='red', width=1)
-SCATTER_LINE_LINE_GREEN = dict(color='green', width=1)
-SCATTER_LINE_LINE_BLUE = dict(color='blue', width=1)
-SCATTER_LINE_LINE_MAGENTA = dict(color='magenta', width=1)
 
-SCATTER_LINE_DASH_BLACK = dict(color='black', dash='dash', width=1)
-SCATTER_LINE_DASH_RED = dict(color='red', dash='dash', width=1)
-SCATTER_LINE_DASH_GREEN = dict(color='green', dash='dash', width=1)
-SCATTER_LINE_DASH_BLUE = dict(color='blue', dash='dash', width=1)
-SCATTER_LINE_DASH_MAGENTA = dict(color='magenta', dash='dash', width=1)
+SCATTER_LINE_LINE_BLACK = dict(color="black", width=1)
+SCATTER_LINE_LINE_RED = dict(color="red", width=1)
+SCATTER_LINE_LINE_GREEN = dict(color="green", width=1)
+SCATTER_LINE_LINE_BLUE = dict(color="blue", width=1)
+SCATTER_LINE_LINE_MAGENTA = dict(color="magenta", width=1)
 
-SCATTER_LINE_DOT_BLACK = dict(color='black', dash='dot', width=1)
-SCATTER_LINE_DOT_RED = dict(color='red', dash='dot', width=1)
-SCATTER_LINE_DOT_GREEN = dict(color='green', dash='dot', width=1)
-SCATTER_LINE_DOT_BLUE = dict(color='blue', dash='dot', width=1)
-SCATTER_LINE_DOT_MAGENTA = dict(color='magenta', dash='dot', width=1)
+SCATTER_LINE_DASH_BLACK = dict(color="black", dash="dash", width=1)
+SCATTER_LINE_DASH_RED = dict(color="red", dash="dash", width=1)
+SCATTER_LINE_DASH_GREEN = dict(color="green", dash="dash", width=1)
+SCATTER_LINE_DASH_BLUE = dict(color="blue", dash="dash", width=1)
+SCATTER_LINE_DASH_MAGENTA = dict(color="magenta", dash="dash", width=1)
 
-SCATTER_LINE_DASHDOT_BLACK = dict(color='black', dash='dashdot', width=1)
-SCATTER_LINE_DASHDOT_RED = dict(color='red', dash='dashdot', width=1)
-SCATTER_LINE_DASHDOT_GREEN = dict(color='green', dash='dashdot', width=1)
-SCATTER_LINE_DASHDOT_BLUE = dict(color='blue', dash='dashdot', width=1)
-SCATTER_LINE_DASHDOT_MAGENTA = dict(color='magenta', dash='dashdot', width=1)
+SCATTER_LINE_DOT_BLACK = dict(color="black", dash="dot", width=1)
+SCATTER_LINE_DOT_RED = dict(color="red", dash="dot", width=1)
+SCATTER_LINE_DOT_GREEN = dict(color="green", dash="dot", width=1)
+SCATTER_LINE_DOT_BLUE = dict(color="blue", dash="dot", width=1)
+SCATTER_LINE_DOT_MAGENTA = dict(color="magenta", dash="dot", width=1)
+
+SCATTER_LINE_DASHDOT_BLACK = dict(color="black", dash="dashdot", width=1)
+SCATTER_LINE_DASHDOT_RED = dict(color="red", dash="dashdot", width=1)
+SCATTER_LINE_DASHDOT_GREEN = dict(color="green", dash="dashdot", width=1)
+SCATTER_LINE_DASHDOT_BLUE = dict(color="blue", dash="dashdot", width=1)
+SCATTER_LINE_DASHDOT_MAGENTA = dict(color="magenta", dash="dashdot", width=1)
 
 
 def build_graph_dgr(las):
     data, yaxis, yaxis2 = [], None, None
 
-    if 'BTVPVS' in las.curves:
+    if "BTVPVS" in las.curves:
         btvpvs_trace, btvpvs_axis = build_trace(
-            las.curves['BTVPVS'],
+            las.curves["BTVPVS"],
             line=SCATTER_LINE_LINE_MAGENTA,
-            yaxis='y'
+            yaxis="y"
         )
         data.append(btvpvs_trace)
         yaxis = btvpvs_axis
 
-    if 'DGRC' in las.curves:
+    if "DGRC" in las.curves:
         dgrc_trace, dgrc_axis = build_trace(
-            las.curves['DGRC'],
+            las.curves["DGRC"],
             line=SCATTER_LINE_LINE_GREEN,
-            yaxis='y2' if yaxis else 'y'
+            yaxis="y2" if yaxis else "y"
         )
         data.append(dgrc_trace)
         if yaxis:
@@ -222,27 +364,27 @@ def build_graph_dgr(las):
         else:
             yaxis = dgrc_axis
 
-    return build_graph('las-curves-dgr', las, data, yaxis, yaxis2)
+    return build_graph("las-curves-dgr", las, data, yaxis, yaxis2)
 
 
 def build_graph_rp(las):
     data, yaxis, yaxis2 = [], None, None
 
-    if 'EWXT' in las.curves:
+    if "EWXT" in las.curves:
         ewxt_trace, ewxt_axis = build_trace(
-            las.curves['EWXT'],
+            las.curves["EWXT"],
             line=SCATTER_LINE_DASHDOT_MAGENTA,
-            yaxis='y'
+            yaxis="y"
         )
-        ewxt_axis['type'] = 'log'
+        ewxt_axis["type"] = "log"
         data.append(ewxt_trace)
         yaxis = ewxt_axis
 
     rp_axis = dict(
-        title='RxxP [ohmm]',
-        type='log'
+        title="RxxP [ohmm]",
+        type="log"
     )
-    rp_keys = ['R09P', 'R15P', 'R27P', 'R39P']
+    rp_keys = ["R09P", "R15P", "R27P", "R39P"]
     rp_lines = [
         SCATTER_LINE_DASHDOT_GREEN,
         SCATTER_LINE_LINE_BLACK,
@@ -256,7 +398,7 @@ def build_graph_rp(las):
             rp_trace, _ = build_trace(
                 las.curves[key],
                 line=line,
-                yaxis='y2' if yaxis else 'y'
+                yaxis="y2" if yaxis else "y"
             )
             data.append(rp_trace)
             has_rp = True
@@ -267,26 +409,26 @@ def build_graph_rp(las):
         else:
             yaxis = rp_axis
 
-    return build_graph('las-curves-rp', las, data, yaxis, yaxis2)
+    return build_graph("las-curves-rp", las, data, yaxis, yaxis2)
 
 
 def build_graph_ald(las):
     data, yaxis, yaxis2 = [], None, None
 
-    if 'ALDCLC' in las.curves:
+    if "ALDCLC" in las.curves:
         aldclc_trace, aldclc_axis = build_trace(
-            las.curves['ALDCLC'],
+            las.curves["ALDCLC"],
             line=SCATTER_LINE_DASH_BLACK,
-            yaxis='y'
+            yaxis="y"
         )
         data.append(aldclc_trace)
         yaxis = aldclc_axis
 
-    if 'ALCDLC' in las.curves:
+    if "ALCDLC" in las.curves:
         alcdlc_trace, alcdlc_axis = build_trace(
-            las.curves['ALCDLC'],
+            las.curves["ALCDLC"],
             line=SCATTER_LINE_LINE_RED,
-            yaxis='y2' if yaxis else 'y'
+            yaxis="y2" if yaxis else "y"
         )
         data.append(alcdlc_trace)
         if yaxis:
@@ -294,41 +436,41 @@ def build_graph_ald(las):
         else:
             yaxis = alcdlc_axis
 
-    return build_graph('las-curves-ald', las, data, yaxis, yaxis2)
+    return build_graph("las-curves-ald", las, data, yaxis, yaxis2)
 
 
 def build_graph_ctn(las):
     data, yaxis = [], None
 
-    if 'TNPS' in las.curves:
+    if "TNPS" in las.curves:
         tnps_trace, tnps_axis = build_trace(
-            las.curves['TNPS'],
+            las.curves["TNPS"],
             line=SCATTER_LINE_LINE_BLUE,
-            yaxis='y'
+            yaxis="y"
         )
         data.append(tnps_trace)
         yaxis = tnps_axis
 
-    return build_graph('las-curves-ctn', las, data, yaxis)
+    return build_graph("las-curves-ctn", las, data, yaxis)
 
 
 def build_graph_bat(las):
     data, yaxis, yaxis2 = [], None, None
 
-    if 'BTCSS' in las.curves:
+    if "BTCSS" in las.curves:
         btcss_trace, btcss_axis = build_trace(
-            las.curves['BTCSS'],
+            las.curves["BTCSS"],
             line=SCATTER_LINE_LINE_MAGENTA,
-            yaxis='y'
+            yaxis="y"
         )
         data.append(btcss_trace)
         yaxis = btcss_axis
 
-    if 'BTCS' in las.curves:
+    if "BTCS" in las.curves:
         btcs_trace, btcs_axis = build_trace(
-            las.curves['BTCS'],
+            las.curves["BTCS"],
             line=SCATTER_LINE_LINE_GREEN,
-            yaxis='y2' if yaxis else 'y'
+            yaxis="y2" if yaxis else "y"
         )
         data.append(btcs_trace)
         if yaxis:
@@ -336,18 +478,18 @@ def build_graph_bat(las):
         else:
             yaxis = btcs_axis
 
-    return build_graph('las-curves-bat', las, data, yaxis, yaxis2)
+    return build_graph("las-curves-bat", las, data, yaxis, yaxis2)
 
 
 def build_trace(curve, **kwargs):
     key = curve.original_mnemonic
     data = curve.data
-    unit = ' [{0}]'.format(curve.unit) if curve.unit != 'NONE' else ''
+    unit = " [{0}]".format(curve.unit) if curve.unit != "NONE" else ""
     descr = curve.descr
 
     trace = go.Scatter(
         y=data,
-        mode='lines',
+        mode="lines",
         name=descr,
         **kwargs
     )
@@ -365,33 +507,33 @@ def build_graph(id, las, data, yaxis=None, yaxis2=None):
 
     x = las.depth_ft
     xaxis = dict(
-        title='depth [ft]'
+        title="depth [ft]"
     )
 
     for trace in data:
-        trace['x'] = x
+        trace["x"] = x
 
     if yaxis2 is not None:
         # since yaxis2 is drawn on top of yaxis
-        yaxis['side'] = 'right'
-        yaxis['showgrid'] = False
+        yaxis["side"] = "right"
+        yaxis["showgrid"] = False
 
     # Note: xaxis, yaxis and yaxis2 must be set before creating layout.
     layout = go.Layout(
-        hovermode='x',
-        paper_bgcolor='rgba(0,0,0,0)',
-        plot_bgcolor='rgba(0,0,0,0)',
+        hovermode="x",
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
         showlegend=True,
         xaxis=xaxis,
         yaxis=yaxis
     )
 
     if yaxis2 is not None:
-        layout['yaxis2'] = yaxis2
+        layout["yaxis2"] = yaxis2
 
     return dcc.Graph(
-        figure={'data': data, 'layout': layout},
-        style={'height': '120mm'},
+        figure={"data": data, "layout": layout},
+        style={"height": "115mm"},
         id=id
     )
 
@@ -412,10 +554,5 @@ def paginate(items, items_per_page):
     return pages
 
 
-def build_page(header, title, contents, className):
-    page = header + [html.H2(title), contents]
-    return html.Div(page, className=className+' page')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
