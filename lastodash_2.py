7f2a57a58b373029d9d385a74050cf6863dd9078
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-01-22 12:14:41 -0500

    Add secondary axes for overlaid subplots.

--- lastodash_2.py
+++ lastodash_2.py
@@ -6,10 +6,12 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 
+from plotly import tools
 import plotly.graph_objs as go
 
 import lasio
 
+import re
 
 app = dash.Dash(__name__)
 
@@ -40,10 +42,13 @@ def parse_args():
 
 
 lasfile, debug = parse_args()
+lf = lasio.read(lasfile)
 
+for curve in lf.curves:
+    print(curve.mnemonic)
+    
 
 def generate_frontpage():
-    lf = lasio.read(lasfile)
 
     filename = os.path.basename(lasfile.name)
     
@@ -65,8 +70,83 @@ def generate_frontpage():
     
     return frontpage
 
+
+def generate_curves():
+    # include one graph for all curves, since they have the same x axis
+    yvals = 'DEPT'
+
+    cols = list(lf.curves.keys())
+
+    plots = []
+    
+    plots.append(['BTVPVS', 'DGRC'])
+    plots.append(list(filter(
+        lambda x: x == 'EWXT' or re.search(r'R[0-9][0-9]P', x),
+        cols)
+    ))
+    plots.append(['ALCDLC', 'ALDCLC'])
+    plots.append(['TNPS'])
+    plots.append(['BTCSS', 'BTCS'])
+    
+    fig = tools.make_subplots(rows=1, cols=len(plots),
+                              shared_yaxes=True)
+
+    for i in range(len(plots)):
+        for column in plots[i]: 
+            fig.append_trace(go.Scatter(
+                x=lf.curves[column].data,
+                y=lf.curves[yvals].data,
+                name=column,
+                line={'width': 1}
+            ), row=1, col=i+1)
+            fig['layout']['xaxis{}'.format(i+1)].update(
+                showgrid=False,
+                zeroline=False,
+                type='log' if column in plots[1] else 'linear'
+            )
+
+    fig['layout'].update(
+        height=800,
+        hovermode='y'
+    )
+
+    fig['data'][1]['xaxis'] = 'x6'
+    fig['data'][6]['xaxis'] = 'x7'
+    fig['data'][8]['xaxis'] = 'x8'
+    fig['data'][11]['xaxis'] = 'x9'
+
+    fig['layout']['xaxis6'] = dict(
+        overlaying='x1',
+        anchor='y',
+        side='top'
+    )
+
+    fig['layout']['xaxis7'] = dict(
+        overlaying='x2',
+        anchor='y',
+        side='top'
+    )
+
+    fig['layout']['xaxis8'] = dict(
+        overlaying='x3',
+        anchor='y',
+        side='top'
+    )
+
+    fig['layout']['xaxis9'] = dict(
+        overlaying='x5',
+        anchor='y',
+        side='top'
+    )
         
+    return dcc.Graph(figure=fig)
+
+
 if __name__ == '__main__':
-    app.layout = html.Div(id='frontpage', className='page',
-                          children=generate_frontpage())
+    app.layout = html.Div([
+        html.Div(id='frontpage', className='page',
+                 children=generate_frontpage()),
+        html.Div(generate_curves())
+    ])
+    
     app.run_server(debug=debug)
